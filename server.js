d8c9a82265ca2f8f6272d6cca4969909d839453a
Adeyinka Micheal <madeyinka6@gmail.com>
2018-07-08 21:58:21 +0100

    added models

--- server.js
+++ server.js
@@ -3,10 +3,18 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns = require('dns');
 
 var cors = require('cors');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
@@ -29,6 +37,33 @@ app.get('/', function(req, res){
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+// main entry point
+app.post("/api/shorturl/new", (req, res) => {
+  let data = req.body;
+  console.log('data', data);
+  let url = data.url;
+  console.log('url', url);
+  // dns.lookup(url, (err, address, family) =>
+  // console.log('address: %j family: IPv%s', address, family
+  // ));
+
+  let splitUrl = url.split('//');
+  console.log('splitUrl', splitUrl)
+  let realUrl = splitUrl[1];
+
+  dns.lookup(realUrl, (err, address, family) => {
+    console.log('address', address);
+    if (address === undefined) {
+      return res.send({
+        "error": "invalid URL"
+      })
+    }
+    console.log('family', family);
+    console.log('err', err);
+    res.send('Data collected!');
+  })
 });
 
 
